cd95da23f9081255b458a5d6ee8de307933f1037
Arno Haase <arno.haase@contrastsecurity.com>
2022-05-02 15:42:07 +0200

    serde

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,29 @@
+use std::fs::File;
+use std::path::Path;
+
 mod intro;
 mod structs;
 mod lifetimes;
 
-fn main() {
-    intro::intro();
-    structs::structs();
+fn main() -> std::io::Result<()> {
+    // intro::intro();
+    // structs::structs();
+    // lifetimes::lifetimes();
+
+    let persons = read_persons(Path::new("persons.json"))?;
+    println!("{:?}", persons);
+
+    Ok(())
+}
+
+#[derive(Debug, serde::Deserialize)]
+struct Person {
+    id: usize,
+    name: String,
+}
+
+fn read_persons(path: &Path) -> std::io::Result<Vec<Person>> {
+    let f = File::open(path)?;
+    let persons = serde_json::from_reader(&f)?;
+    Ok(persons)
 }
