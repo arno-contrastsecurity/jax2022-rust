49e4f4f2df7638e7db0412d7c72945a3d6a63b39
Arno Haase <arno.haase@contrastsecurity.com>
2022-05-02 16:23:13 +0200

    clap

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,36 @@ mod structs;
 mod lifetimes;
 mod io;
 
+use clap::Parser;
+
+
 fn main() -> std::io::Result<()> {
-    intro::intro();
-    structs::structs();
-    lifetimes::lifetimes();
-    io::io()?;
+
+    let args = Args::parse();
+
+    // intro::intro();
+    // structs::structs();
+    // lifetimes::lifetimes();
+    // io::io()?;
 
     Ok(())
+}
+
+
+#[derive(Parser)]
+#[clap(name = "movies", long_about = Some("This CLI tool provides query access to the movie database."))]
+/// movies is an application for finding movies
+struct Args {
+    #[clap(arg_enum, long="The genre to look for")]
+    genre: Genre,
+}
+
+
+
+
+#[derive(Clone, clap::ArgEnum)]
+enum Genre {
+    Action,
+    Comedy,
+    Romance,
 }
